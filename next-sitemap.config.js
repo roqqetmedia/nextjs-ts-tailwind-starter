9a01fca1dbebf4992126c6927b76dabc09ce386e
Ali Piry <ali@piry.dev>
2023-01-17 22:06:49 +0330

    update siteUrl on next sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://roqqetmedia.com",
+  siteUrl:
+    process.env.NEXT_PUBLIC_SITE_URL || "https://nextjs-starter.roqqet.media",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
