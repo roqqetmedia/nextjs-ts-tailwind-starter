e8af16ae7c99d3d520b27eba0593353d984bdb22
Ali Piry <ali@piry.dev>
2023-08-07 12:29:13 +0330

    omit X-Robots-Tag to noindex on vercel dev/preview envs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,24 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    const headers = [];
+
+    if (process.env.APP_ENV !== "production") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+
+        source: "/:path*",
+      });
+    }
+
+    return headers;
+  },
 };
 
 module.exports = nextConfig;
